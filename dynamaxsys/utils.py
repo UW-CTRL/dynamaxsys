1edf13984e07afada8532562a85ed4fcfc21a52a
Karen Leung <karen.ym.leung@gmail.com>
2025-10-31 11:49:43 -0700

    added default time=0 (#6)

--- dynamaxsys/utils.py
+++ dynamaxsys/utils.py
@@ -1,12 +1,11 @@
 import jax
-import functools
 import equinox as eqx
 import jax.numpy as jnp
 
 @eqx.filter_jit
 def runge_kutta_integrator(dynamics, dt=0.1):
     # zero-order hold
-    def integrator(x, u, d, t):
+    def integrator(x, u, d=None, t=0.0):
         dt2 = dt / 2.0
         k1 = dynamics(x, u, d, t)
         k2 = dynamics(x + dt2 * k1, u, d, t + dt2)
@@ -15,11 +14,10 @@ def runge_kutta_integrator(dynamics, dt=0.1):
         return x + (dt / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
     return integrator
 
-# @functools.partial(jax.jit, static_argnames=["dynamics"])
 @eqx.filter_jit
-def linearize(dynamics, state, control, disturbance, t):
+def linearize(dynamics, state, control, disturbance=None, time=0.0):
     if disturbance is None:
         disturbance = jnp.zeros((dynamics.disturbance_dim,))
-    A, B, C = jax.jacobian(dynamics, [0, 1, 2])(state, control, disturbance, t)
-    D = dynamics(state, control, disturbance, t) - A @ state - B @ control - C @ disturbance
+    A, B, C = jax.jacobian(dynamics, [0, 1, 2])(state, control, disturbance, time)
+    D = dynamics(state, control, disturbance, time) - A @ state - B @ control - C @ disturbance
     return A, B, C, D
