9acc447abf18eaf982891e1388c121242172bba5
Karen Leung <karen.ym.leung@gmail.com>
2025-12-06 00:24:39 -0800

    karen/fix relative systems (#7)
    * fix relative unicycle
    
    * fix relative simplecar
    
    * fix relative simplecar change naming
    
    * fix velocity limits
    
    * add velocity limits

--- dynamaxsys/unicycle.py
+++ dynamaxsys/unicycle.py
@@ -5,6 +5,14 @@ from dynamaxsys.base import ControlAffineDynamics, ControlDisturbanceAffineDynam
 class Unicycle(ControlAffineDynamics):
     state_dim: int = 3
     control_dim: int = 2
+    """ Unicycle model with state [x, y, theta] and control [v, omega]
+    where x,y is the position, theta is the heading angle,
+    v is the linear velocity, and omega is the angular velocity.
+    The dynamics are given by:
+        dx/dt = v * cos(theta)
+        dy/dt = v * sin(theta)
+        dtheta/dt = omega
+    """
 
     def __init__(self):
         def drift_dynamics(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
@@ -23,16 +31,26 @@ class Unicycle(ControlAffineDynamics):
 class DynamicallyExtendedUnicycle(ControlAffineDynamics):
     state_dim: int = 4
     control_dim: int = 2
+    min_max_velocity: tuple
+    """ Dynamically extended unicycle model with state [x, y, theta, v] and control [a, omega]
+    where x,y is the position, theta is the heading angle,
+    v is the linear velocity, a is the linear acceleration, and omega is the angular velocity.
+    The dynamics are given by:
+        dx/dt = v * cos(theta)
+        dy/dt = v * sin(theta)
+        dtheta/dt = omega
+        dv/dt = a
+    """
 
-    def __init__(self):
+    def __init__(self, min_max_velocity: tuple = (-jnp.inf, jnp.inf)):
+        self.min_max_velocity = min_max_velocity
+
         def drift_dynamics(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
-            x, y, th, v = state
-            # v, om = control
+            _, _, th, v = state
+            v = jnp.clip(v, *self.min_max_velocity)
             return jnp.array([v * jnp.cos(th), v * jnp.sin(th), 0.0, 0.0])
 
         def control_jacobian(state, time: float = 0.0) -> jnp.ndarray:
-            x, y, th, v = state
-            # v, om = control
             return jnp.array([[0.0, 0.0], [0.0, 0.0], [1.0, 0.0], [0.0, 1.0]])
 
         super().__init__(
@@ -44,13 +62,17 @@ class RelativeUnicycle(ControlDisturbanceAffineDynamics):
     state_dim: int = 3
     control_dim: int = 2
     disturbance_dim: int = 2
-
-    # def ode_dynamics(self, state, control, time=0):
-    #     xrel, yrel, threl = state
-    #     v1, om1, v2, om2 = control
-    #     return jnp.array([v2 * jnp.cos(threl) + om1 * yrel - v1,
-    #                       v2 * jnp.sin(threl) - om1 * xrel,
-    #                       om2 - om1])
+    """ Relative unicycle dynamics with state [x_rel, y_rel, theta_rel],
+    control [v1, omega1] and disturbance [v2, omega2],
+    where (x_rel, y_rel) is the position of unicycle 2 relative to unicycle 1,
+    theta_rel is the heading of unicycle 2 relative to unicycle 1,
+    v1, omega1 are the linear and angular velocities of unicycle 1,
+    and v2, omega2 are the linear and angular velocities of unicycle 2.
+    The dynamics are given by:
+        dx_rel/dt = v2 * cos(theta_rel) + omega1 * y_rel - v1
+        dy_rel/dt = v2 * sin(theta_rel) - omega1 * x_rel
+        dtheta_rel/dt = omega2 - omega1
+    """
 
     def __init__(self):
         def drift_dynamics(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
@@ -94,11 +116,34 @@ class RelativeDynamicallyExtendedUnicycle(ControlDisturbanceAffineDynamics):
     state_dim: int = 5
     control_dim: int = 2
     disturbance_dim: int = 2
+    min_max_velocity_ego: tuple
+    min_max_velocity_contender: tuple
+    """ Relative dynamically extended unicycle dynamics with state [x_rel, y_rel, theta_rel, v1, v2],
+    control [a1, omega1] and disturbance [a2, omega2],
+    where (x_rel, y_rel) is the position of unicycle 2 relative to unicycle 1,
+    theta_rel is the heading of unicycle 2 relative to unicycle 1,
+    v1, a1 are the linear velocity and acceleration of unicycle 1,
+    and v2, a2 are the linear velocity and acceleration of unicycle 2.
+    The dynamics are given by:
+        dx_rel/dt = v2 * cos(theta_rel) + omega1 * y_rel - v1
+        dy_rel/dt = v2 * sin(theta_rel) - omega1 * x_rel
+        dtheta_rel/dt = omega2 - omega1
+        dv1/dt = a1
+        dv2/dt = a2
+    """
 
-    def __init__(self):
+    def __init__(
+        self,
+        min_max_velocity_ego: tuple = (-jnp.inf, jnp.inf),
+        min_max_velocity_contender: tuple = (-jnp.inf, jnp.inf),
+    ):
+        self.min_max_velocity_ego = min_max_velocity_ego
+        self.min_max_velocity_contender = min_max_velocity_contender
+
         def drift_dynamics(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
             xrel, yrel, threl, v1, v2 = state
-            # om1, a1, om2, a1 = control
+            v1 = jnp.clip(v1, *self.min_max_velocity_ego)
+            v2 = jnp.clip(v2, *self.min_max_velocity_contender)
             return jnp.array(
                 [
                     v2 * jnp.cos(threl) - v1,
@@ -110,12 +155,12 @@ class RelativeDynamicallyExtendedUnicycle(ControlDisturbanceAffineDynamics):
             )
 
         def control_jacobian(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
-            xrel, yrel, threl, v1, v2 = state
-            # om1, a1, om2, a1 = control
+            xrel, yrel, _, _, _ = state
+            # om1, a1 = control
             return jnp.array(
                 [
                     [yrel, 0.0],
-                    [xrel, 0.0],
+                    [-xrel, 0.0],
                     [-1.0, 0.0],
                     [0.0, 1.0],
                     [0.0, 0.0],
@@ -123,7 +168,7 @@ class RelativeDynamicallyExtendedUnicycle(ControlDisturbanceAffineDynamics):
             )
 
         def disturbance_jacobian(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
-            xrel, yrel, threl, v1, v2 = state
+            # xrel, yrel, threl, v1, v2 = state
             # om2, a2 = disturbance
             return jnp.array(
                 [
