6632b96c0e88a2cbb70d84f827b06f4c43e04ef8
kaleung <kaleung@nvidia.com>
2026-02-17 12:04:05 -0800

    added parametric integrator

--- dynamaxsys/integrators.py
+++ dynamaxsys/integrators.py
@@ -2,6 +2,7 @@ import jax.numpy as jnp
 from dynamaxsys.base import (
     LinearControlDynamics,
     LinearControlDisturbanceDynamics,
+    ControlAffineDynamics,
 )
 
 
@@ -52,3 +53,35 @@ class TwoPlayerRelativeIntegratorND(LinearControlDisturbanceDynamics):
         B = jnp.zeros([state_dim, self.N_dim])
         B = B.at[-self.N_dim :].set(jnp.eye(self.N_dim))
         super().__init__(A, -B, B)
+
+
+class ParametricIntegratorND(ControlAffineDynamics):
+    integrator_dim: int
+    N_dim: int
+
+    def __init__(self, integrator_dim, N_dim):
+        self.integrator_dim = integrator_dim
+        self.N_dim = N_dim
+        state_dim = self.integrator_dim * self.N_dim
+        control_dim = self.N_dim
+
+        A = jnp.eye(state_dim, k=self.N_dim)
+        A_parameter = jnp.concatenate(
+            [
+                jnp.concatenate([A, jnp.zeros([1, state_dim])], axis=0),
+                jnp.zeros([state_dim + 1, 1]),
+            ],
+            axis=1,
+        )
+        B = jnp.zeros([state_dim, control_dim])
+        B = B.at[-self.N_dim :].set(jnp.eye(self.N_dim))
+        B_parameter = jnp.concatenate([B, jnp.zeros([1, control_dim])], axis=0)
+
+        def drift_dynamics(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
+            return A_parameter @ state
+
+        def control_jacobian(state: jnp.ndarray, time: float = 0.0) -> jnp.ndarray:
+            alpha = state[-1]
+            return B_parameter * alpha
+
+        super().__init__(drift_dynamics, control_jacobian, state_dim + 1, control_dim)
